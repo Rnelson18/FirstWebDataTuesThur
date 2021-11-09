fa673ff442a4df72bac413ff5816c3208c755d75
ryannelson <r3nelson@student.bridgew.edu>
2021-11-09 09:56:40 -0500

    Initial commit

---
+++ src/bsu/comp152/Main.java
@@ -0,0 +1,37 @@
+package bsu.comp152;
+
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) {
+	var dataGrabber = HttpClient.newHttpClient();
+    var requestBuilder = HttpRequest.newBuilder();
+    var webURI = URI.create("http://universities.hipolabs.com/searches?name=Young");
+    var webRequest= requestBuilder.uri(webURI).build();
+    HttpResponse<String> response = null;
+        try{
+        response= dataGrabber.send(webRequest, HttpResponse.BodyHandlers.ofString());
+    }
+    catch(IOException exceptionObject){
+        System.out.println("Error connecting to site or network is down");
+    }
+    catch(InterruptedException e){
+        System.out.println("Connection to server interrupted = cowerdly giving up now");
+    }
+    if(response == null){
+        System.out.println("Something went wrong getting data from the network");
+        System.exit(1);
+    }
+    var usefulData = response.body();
+    var dataParser = new Gson();
+    UniversityDataType[] uniList = dataParser.fromJson(usefulData, UniversityDataType[].class);
+    System.out.println(usefulData);
+    }
+}
